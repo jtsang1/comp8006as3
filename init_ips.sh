8651edffc743a9bb88ef715162763596e81ce89c
KevinEngRepo <k.eng2991@gmail.com>
2014-03-12 11:35:18 -0700

    Update init_ips.sh
    Included permission edit for ips.sh. When initializing bootstrap on any other machine, would be mailed the permission denied message from crontab.

--- init_ips.sh
+++ init_ips.sh
@@ -7,11 +7,13 @@ iptables -X
 # Make paths and copy files to path
 mkdir /root/Documents
 mkdir /root/Documents/ips_ssh
+chmod 777 /root/Documents/ips_ssh
 cp ./main.py /root/Documents/ips_ssh/
 cp ./file_init.sh /root/Documents/ips_ssh/
 cp ./file_tail.sh /root/Documents/ips_ssh/
 cp ./file_reset.sh /root/Documents/ips_ssh/
 cp ./ips.sh /root/Documents/ips_ssh/
+chmod 777 /root/Documents/ips_ssh/ips.sh
 
 # Init files
 . /root/Documents/ips_ssh/file_init.sh
